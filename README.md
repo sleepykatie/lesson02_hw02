7ad0e2d6f3357591426493bc7e95c9f220f9206f
Ekaterina <mikhaylina_kate@mail.ru>
2022-04-13 21:05:30 +0300

    попытка pull request №3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lesson02_hw_02
 Попытка создания pull request №2.
+
+Попытка создания правильного pull request №3.
--- task1.py
+++ task1.py
@@ -4,3 +4,4 @@ print(type(15 // 2))
 print(type(15 ** 2))
 
 # changes for pull request
+# more changes for pull request
