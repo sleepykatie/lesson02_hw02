7e13c9b56a142884dfb2b0413526c2943c57ec54
Ekaterina <mikhaylina_kate@mail.ru>
2022-04-23 00:14:06 +0300

    ДЗ 5, N1-2

--- task1.py
+++ task1.py
@@ -1,4 +1,12 @@
-print(type(15 * 3))
-print(type(15 / 3))
-print(type(15 // 2))
-print(type(15 ** 2))
+def odd_numbers(n):
+    for num in range(1, n + 1, 2):
+        yield num
+
+
+if __name__ == '__main__':
+    #print(odd_numbers(10))
+    gen_list_odd = odd_numbers(10)
+    print(next(gen_list_odd))
+    print(next(gen_list_odd))
+    print(next(gen_list_odd))
+    #print(next(gen_list_odd))
