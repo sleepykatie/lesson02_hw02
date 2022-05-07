4c979943dae20c5d06ddba89a85c9eb9b69ca78e
Ekaterina <mikhaylina_kate@mail.ru>
2022-05-07 17:56:16 +0300

    ДЗ №7, задания №1, 2.

--- task2.py
+++ task2.py
@@ -1,28 +1,53 @@
-my_list = ['в', '5', 'часов', '17', 'минут', 'температура',
-           'воздуха', 'была', '+5', 'градусов']
+import os
+import yaml
 
-for i in range(len(my_list)):
-    element = my_list[i]
-    marker = element.isdigit()
-    if marker:
-        value = int(element)
-        my_list[i] = ('{:0>2d}'.format(value))
-        my_list[i] = f'"{my_list[i]}"'
-    else:
-        value_aux = ''
-        for s in element:
-            marker_symbol = s.isdigit()
-            if marker_symbol:
-                value = int(s)
-                symbol = ('{:0>2d}'.format(value))
-                s = str(symbol)
-            value_aux += s
-            if marker_symbol:
-                my_list[i] = f'"{value_aux}"'
 
-answer = ''
-for i in range(len(my_list)):
-    answer = answer + my_list[i] + ' '
-print(answer)
+def dict_proc(current_dict, current_dict_path):
 
+    for key in current_dict:
+        # print('makedir', current_dict_path)
+        os.makedirs(current_dict_path, exist_ok=True)
+        element = current_dict[key]
+        if type(element) == list:
+            current_path = os.path.join(current_dict_path, key)
+            os.makedirs(current_path, exist_ok=True)
+            # print('makedir', current_path)
+            list_proc(element, current_path)
+        elif type(element) == dict:
+            current_path = os.path.join(current_dict_path, key)
+            dict_proc(element, current_path)
+
+
+def list_proc(current_list, current_list_path):
+    for el in current_list:
+        if type(el) == dict:
+            dict_proc(el, current_list_path)
+        elif type(el) == str:
+            # print('makefile', current_list_path)
+            open(os.path.join(current_list_path, el), 'w').close()
+
+
+if __name__ == '__main__':
+    root_path = os.path.join(os.getcwd(), 'task2')
+    os.makedirs(root_path, exist_ok=True)
+    with open(os.path.join(root_path, 'config.yaml'), 'r') as f:
+        # config = yaml.safe_load(f)
+        # config = yaml.load(f, Loader=yaml.FullLoader)
+        config = yaml.full_load(f)
+
+    dict_proc(config, root_path)
+
+
+'''
+{'my_project': 
+    {'settings': ['__init__.py', 'dev.py', 'prod.ru'], 
+    'mainapp': ['__init__.py', 'models.py', 'views.py', 
+        {'templates': [{'mainapp': ['base.html', 'index.html']}]}
+        ], 
+    'authapp': ['__init__.py', 'models.py', 'views.py', 
+        {'templates': [{'authapp': ['base.html', 'index.html']}]}
+        ]
+        }
+        }
+'''
 
