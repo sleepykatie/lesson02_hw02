7cea1812a1003523c3bb3e7140848a4df9163c75
Ekaterina <mikhaylina_kate@mail.ru>
2022-05-22 21:45:55 +0300

    ДЗ №9, задание №2 изм.

--- task2.py
+++ task2.py
@@ -1,19 +1,22 @@
 class Road:
-    _length = 1000000
-    _width = 9
+    def __init__(self, length, width):
+        self._length = length   # m
+        self._width = width     # m
 
-    def calc_asphalt(self, thickness=8, dim_thickness='cm'):
-        density_av = 2400
+    def calc_asphalt(self, thickness, dim_thickness, density_av=2400):
+        # density_av = 2400
         # dim_density_av = 'kg/m^3'
         if dim_thickness == 'mm':
             thickness /= 1000
         elif dim_thickness == 'cm':
             thickness /= 100
+        elif dim_thickness == 'm':
+            thickness = thickness
         v_asphalt = self._length * self._width * thickness
         m_asphalt = (v_asphalt * density_av)/1000
 
         return f'{m_asphalt} t'
 
 
-interstate_60 = Road()
-print(interstate_60.calc_asphalt(15, 'cm'), )
+interstate_60 = Road(1000000, 9)
+print(interstate_60.calc_asphalt(15, 'cm'))
