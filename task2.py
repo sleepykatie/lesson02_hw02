559fac9912dd52ff84f23e4c445812444ec0bf5a
Ekaterina <mikhaylina_kate@mail.ru>
2022-06-04 17:22:24 +0300

    ДЗ №10, задание №2.

--- task2.py
+++ task2.py
@@ -1,28 +1,33 @@
-my_list = ['в', '5', 'часов', '17', 'минут', 'температура',
-           'воздуха', 'была', '+5', 'градусов']
-
-for i in range(len(my_list)):
-    element = my_list[i]
-    marker = element.isdigit()
-    if marker:
-        value = int(element)
-        my_list[i] = ('{:0>2d}'.format(value))
-        my_list[i] = f'"{my_list[i]}"'
-    else:
-        value_aux = ''
-        for s in element:
-            marker_symbol = s.isdigit()
-            if marker_symbol:
-                value = int(s)
-                symbol = ('{:0>2d}'.format(value))
-                s = str(symbol)
-            value_aux += s
-            if marker_symbol:
-                my_list[i] = f'"{value_aux}"'
-
-answer = ''
-for i in range(len(my_list)):
-    answer = answer + my_list[i] + ' '
-print(answer)
+from abc import ABC, abstractmethod
 
 
+class Clothes(ABC):
+
+    def __init__(self, main_param):
+        self.main_param = main_param
+
+    @abstractmethod
+    def consumption_fabric(self):
+        pass
+
+
+class Coat(Clothes):
+
+    @property
+    def consumption_fabric(self):
+        return self.main_param/6.5 + 0.5
+
+
+class Suit(Clothes):
+
+    @property
+    def consumption_fabric(self):
+        return 2 * self.main_param + 0.3
+
+
+blue_coat = Coat(46)
+beige_suit = Suit(1.74)
+sum_consump_fabric = blue_coat.consumption_fabric + beige_suit.consumption_fabric
+print(blue_coat.consumption_fabric)
+print(beige_suit.consumption_fabric)
+print(sum_consump_fabric)
