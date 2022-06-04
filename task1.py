09e298570416b277f295dd83f01bfbc7c33e8fbe
Ekaterina <mikhaylina_kate@mail.ru>
2022-06-05 00:51:53 +0300

    ДЗ №11, задание №1.

--- task1.py
+++ task1.py
@@ -1,38 +1,63 @@
-class Matrix:
+import re
+import datetime
+current_date = datetime.datetime.now()
 
-    def __init__(self, matrix_data):
-        self.matrix_data = matrix_data
-        for n in range(0, len(self.matrix_data)):
-            check_length = len(self.matrix_data[0])
-            if len(self.matrix_data[n]) != check_length:
-                raise ValueError('Check data!')
+# re_date = re.compile(r'^(\d{2}-){2}\d{4}$')
+#
+# for date in ['23.01.1990', '12-03-1870', '9-12-784']:
+#     if re_date.match(date):
+#         print(date, "it's OK")
+#     else:
+#         print(f'Wrong date {date}')
 
-    def __str__(self):
-        row = ''
-        for el in self.matrix_data:
-            row += '| '
-            for num in el:
-                row += f'{num} '
-            row += '|\n'
-        return row
 
-    def __add__(self, other):
-        if len(self.matrix_data) != len(other.matrix_data) \
-                or len(self.matrix_data[0]) != len(other.matrix_data[0]):
-            raise ValueError('Matrices must be the same size')
+class Date:
+
+    def __init__(self, date_str):
+        re_date = re.compile(r'^(\d{2}-){2}\d{4}$')
+        if not re_date.match(date_str) and type(date_str) != str:
+            raise ValueError('Wrong format!')
         else:
-            result_matrix = []
-            for i in range(0, len(self.matrix_data)):
-                result_row = []
-                for j in range(0, len(self.matrix_data[i])):
-                    result_el = self.matrix_data[i][j] + other.matrix_data[i][j]
-                    result_row.append(result_el)
-                result_matrix.append(result_row)
-        return Matrix(result_matrix)
+            self.date_str = date_str
+
+    @classmethod
+    def date_to_int(cls, date_str):
+        # re_day = re.compile(r'^\d{2}')
+        # re_month = re.compile(r'-\d{2}')
+        # re_year = re.compile(r'-\d{4}$')
+        # day = re_day.match(date_str)
+        # month = re_month.match(date_str)
+        # year = re_year.match(date_str)
+        date_parts = date_str.split('-')
+        day = int(date_parts[0])
+        month = int(date_parts[1])
+        year = int(date_parts[2])
+
+        return [day, month, year]
+
+    @staticmethod
+    def date_validation(date_list):
+        month_31day = [1, 3, 5, 7, 8, 10, 12]
+        month_30day = [4, 6, 9, 11]
+        if not 1900 <= date_list[2] <= current_date.year:
+            return 'Wrong year!'
+        if not 0 < date_list[1] <= 12:
+            return 'Wrong month!'
+        for month in month_31day:
+            if month == date_list[1]:
+                if not 0 < date_list[0] <= 31:
+                    return 'Wrong day!'
+        for month in month_30day:
+            if month == date_list[1]:
+                if not 0 < date_list[0] <= 30:
+                    return 'Wrong day!'
+        if date_list[1] == 2 and not 0 < date_list[0] <= 29:
+            return 'Wrong day!'
+        else:
+            return 'The date is valid'
 
 
-matrix_01 = Matrix([[3, 3], [2, 2], [4, 4]])
-matrix_02 = Matrix([[1, 1], [1, 1], [1, 1]])
-# print(matrix_01)
-# print(matrix_02)
-print(matrix_01 + matrix_02)
+date_01 = Date('02-02-1980')
+print(date_01.date_to_int('03-03-1970'))
+print(Date.date_to_int('01-01-1995'))
+print(Date.date_validation(date_01.date_to_int('23-12-1999')))
